1b0d059ce2832ee8532663758683c969b2bc1aa8
Jorandeboever <jorandeboever@gmail.com>
2016-12-23 22:11:27 +0100

    Fix scriptjes

--- src/main/resources/db/migration/V20161223_001__add_example_data.sql
+++ src/main/resources/db/migration/V20161223_001__add_example_data.sql
@@ -1,10 +1,10 @@
-INSERT INTO t_person (id, password, username) VALUES (1, 'joran', 'Joran');
+INSERT INTO T_PERSON (ID, PASSWORD, USERNAME) VALUES (1, 'JORAN', 'JORAN');
 
-INSERT INTO t_car (id, brand, fueltype, model, purchasedate, purchaseprice, owner_id) VALUES (1, 'Fiat', null, 'Punto', '2016-09-20', 3750, 1);
+INSERT INTO T_CAR (ID, BRAND, FUELTYPE, MODEL, PURCHASEDATE, PURCHASEPRICE, OWNER_ID) VALUES (1, 'FIAT', NULL, 'PUNTO', '2016-09-20', 3750, 1);
 
-INSERT INTO t_refueling (id, date, kilometres, liters, price, car_id, priceperliter, fueltankfull) VALUES (2, '2016-12-04', 131430, 36.32, 41.01, 1, 1.129, TRUE );
-INSERT INTO t_refueling (id, date, kilometres, liters, price, car_id, priceperliter, fueltankfull) VALUES (3, '2016-11-14', 130771, 38.56, 41.99, 1, 1.089, 1);
-INSERT INTO t_refueling (id, date, kilometres, liters, price, car_id, priceperliter, fueltankfull) VALUES (6, '2016-09-30', 129149, 42.25, 45.59, 1, 1.079, 1);
-INSERT INTO t_refueling (id, date, kilometres, liters, price, car_id, priceperliter, fueltankfull) VALUES (4, '2016-10-27', 130324, 20.54, 23.19, 1, 1.129, 1);
-INSERT INTO t_refueling (id, date, kilometres, liters, price, car_id, priceperliter, fueltankfull) VALUES (5, '2016-10-20', 129789, 14.97, 17.12, 1, 1.144, 1);
-INSERT INTO t_refueling (id, date, kilometres, liters, price, car_id, priceperliter, fueltankfull) VALUES (1, '2016-12-19', 132232, 42.02, 48.7, 1, 1.159, 1);
+INSERT INTO T_REFUELING (ID, DATE, KILOMETRES, LITERS, PRICE, CAR_ID, PRICEPERLITER, FUELTANKFULL) VALUES (2, '2016-12-04', 131430, 36.32, 41.01, 1, 1.129, TRUE );
+INSERT INTO T_REFUELING (ID, DATE, KILOMETRES, LITERS, PRICE, CAR_ID, PRICEPERLITER, FUELTANKFULL) VALUES (3, '2016-11-14', 130771, 38.56, 41.99, 1, 1.089, 1);
+INSERT INTO T_REFUELING (ID, DATE, KILOMETRES, LITERS, PRICE, CAR_ID, PRICEPERLITER, FUELTANKFULL) VALUES (6, '2016-09-30', 129149, 42.25, 45.59, 1, 1.079, 1);
+INSERT INTO T_REFUELING (ID, DATE, KILOMETRES, LITERS, PRICE, CAR_ID, PRICEPERLITER, FUELTANKFULL) VALUES (4, '2016-10-27', 130324, 20.54, 23.19, 1, 1.129, 1);
+INSERT INTO T_REFUELING (ID, DATE, KILOMETRES, LITERS, PRICE, CAR_ID, PRICEPERLITER, FUELTANKFULL) VALUES (5, '2016-10-20', 129789, 14.97, 17.12, 1, 1.144, 1);
+INSERT INTO T_REFUELING (ID, DATE, KILOMETRES, LITERS, PRICE, CAR_ID, PRICEPERLITER, FUELTANKFULL) VALUES (1, '2016-12-19', 132232, 42.02, 48.7, 1, 1.159, 1);
