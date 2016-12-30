2583064843810ae4966b95566df40e314da64714
Joran De Boever <joran.deboever@hpcds.com>
2016-12-30 16:15:24 +0100

    @Immutable voor amountType

--- src/main/java/be/ghostwritertje/domain/AmountType.java
+++ src/main/java/be/ghostwritertje/domain/AmountType.java
@@ -1,5 +1,7 @@
 package be.ghostwritertje.domain;
 
+import org.hibernate.annotations.Immutable;
+
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -7,6 +9,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "T_AMOUNT_TYPE")
 @DiscriminatorColumn(name = "AMOUNT_NAME")
+@Immutable
 public class AmountType extends DomainObject {
     private static final long serialVersionUID = -2979061875257223800L;
 
