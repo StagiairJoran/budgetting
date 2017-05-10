93cf081fc64c690547df49d2f653f0748c8059c2
Jorandeboever <jorandeboever@gmail.com>
2017-05-10 19:32:03 +0200

    Nicer colors!

--- src/main/java/be/ghostwritertje/webapp/charts/PieChartBuilder.java
+++ src/main/java/be/ghostwritertje/webapp/charts/PieChartBuilder.java
@@ -1,11 +1,13 @@
 package be.ghostwritertje.webapp.charts;
 
 import be.ghostwritertje.views.budgetting.CategoryGroupView;
+import be.ghostwritertje.views.budgetting.CategoryView;
 import com.googlecode.wickedcharts.highcharts.options.ChartOptions;
 import com.googlecode.wickedcharts.highcharts.options.DataLabels;
 import com.googlecode.wickedcharts.highcharts.options.PixelOrPercent;
 import com.googlecode.wickedcharts.highcharts.options.SeriesType;
 import com.googlecode.wickedcharts.highcharts.options.color.HexColor;
+import com.googlecode.wickedcharts.highcharts.options.color.HighchartsColor;
 import com.googlecode.wickedcharts.highcharts.options.series.Point;
 import com.googlecode.wickedcharts.highcharts.options.series.PointSeries;
 import com.googlecode.wickedcharts.highcharts.options.series.Series;
@@ -40,23 +42,29 @@ public class PieChartBuilder extends ChartBuilderSupport<PieChartBuilder> {
         this.seriesListConsumer = seriesList -> {
 
             DataLabels dataLabels1 = new DataLabels();
-            dataLabels1
-                    .setColor(new HexColor("#ffffff"));
-            dataLabels1
-                    .setDistance(-30);
+            dataLabels1.setColor(new HexColor("#ffffff"));
+            dataLabels1.setDistance(-30);
 
             PointSeries pointSeries1 = new PointSeries();
             pointSeries1.setType(SeriesType.PIE);
-            categoryGroups.getObject().forEach(o -> pointSeries1.addPoint(new Point(o.getDisplayValue(), (Number) o.getNumberDisplayValue())));
             pointSeries1.setSize(new PixelOrPercent(60, PixelOrPercent.Unit.PERCENT));
             pointSeries1.setDataLabels(dataLabels1);
-            seriesList.add(pointSeries1);
 
             PointSeries pointSeries2 = new PointSeries();
             pointSeries2.setType(SeriesType.PIE);
             pointSeries2.setInnerSize(new PixelOrPercent(60, PixelOrPercent.Unit.PERCENT));
-            categoryGroups.getObject().forEach(o -> o.getCategoryList().forEach(c -> pointSeries2.addPoint(new Point(c.getDisplayValue(), (Number) c.getNumberDisplayValue()))));
             pointSeries2.setDataLabels(new DataLabels());
+
+            int i = 1;
+            for (CategoryGroupView categoryGroup : categoryGroups.getObject()) {
+                pointSeries1.addPoint(new Point(categoryGroup.getDisplayValue(), categoryGroup.getNumberDisplayValue(), new HighchartsColor(++i)));
+                Float brightness = 0.01F;
+                for (CategoryView category : categoryGroup.getCategoryList()) {
+                    pointSeries2.addPoint(new Point(category.getDisplayValue(), category.getNumberDisplayValue(), new HighchartsColor(i).brighten(brightness)));
+                    brightness += 0.05F;
+                }
+            }
+            seriesList.add(pointSeries1);
             seriesList.add(pointSeries2);
 
             return seriesList;
